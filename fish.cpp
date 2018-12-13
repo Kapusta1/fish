f7a208b10de7e34a14d5de97c0119a79fd225db5
tom <tomestrader@gmail.com>
2018-12-14 01:32:55 +0300

    11.1 polimorfism

--- fish.cpp
+++ fish.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
 using namespace std;
 
+class Fish
+{
+public:
+	void Swim()
+	{
+		cout << "Fish Swim!! " << endl;
+	}
+};
+
+class Tuna :public Fish
+{
+public:
+	void Swim()
+	{
+		cout << "Tuna swims! " << endl;
+	}
+};
+
+void MakeFishswim(Fish & inputFish)
+{
+	inputFish.Swim();
+}
+
+int main()
+{
+	Tuna myDinner;
+	myDinner.Swim();
+
+	MakeFishswim(myDinner);
+	return 0;
+}
